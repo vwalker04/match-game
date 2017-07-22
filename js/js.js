39fc8767f573748a3c1a49d6764a79d0a26e0116
Vaughn <vwalker04@yahoo.com>
2017-07-22 14:18:40 -0400

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,160 +1,252 @@
-// An attempt at adding "preload" function to help with performance
-// with images.
-var images = new Array();
-function preload() {
-  for (i = 0; i < preload.arguments.length; i++) {
-    images[i] = new Array();
-    images[i].src = preload.arguments[i];
-  }
-}
-preload (
-  "img/r1.jpg",
-  "img/r2.jpg",
-  "img/r3.jpg",
-  "img/r4.jpg",
-  "img/r5.jpg",
-  "img/r6.jpg",
-  "img/r7.jpg",
-  "img/r8.jpg",
-  "img/r9.jpg",
-  "img/r10.jpg",
-  "img/r11.jpg",
-  "img/r12.jpg"
-)
-// End of preload
+$(document).ready(function() {
+// Box 1
+  var imgS = "img/star1.jpg";
+  $('#box1').mousedown(function () {
+    $(this).attr("src","img/r1.jpg");
+  });
+  $('#box1').mouseup(function () {
+    $(this).attr("src",imgS);
+  });
 
-var imageTracker = "s";
-function switchImg() {
-  var image = document.getElementById('box1');
-  if (imageTracker =="s") {
-    image.src = "img/r1.jpg";
-    imageTracker = "r";
-  } else {
-    image.src = "img/star1.jpg";
-    imageTracker = "s";
-  }
-}
-var imageTracker = "s";
-function switchImg2() {
-  var image = document.getElementById('box2');
-  if (imageTracker =="s") {
-    image.src = "img/r2.jpg";
-    imageTracker = "r";
-  } else {
-    image.src = "img/star1.jpg";
-    imageTracker = "s";
-  }
-}
-var imageTracker = "s";
-function switchImg3() {
-  var image = document.getElementById('box3');
-  if (imageTracker =="s") {
-    image.src = "img/r3.jpg";
-    imageTracker = "r";
-  } else {
-    image.src = "img/star1.jpg";
-    imageTracker = "s";
-  }
-}
-var imageTracker = "s";
-function switchImg4() {
-  var image = document.getElementById('box4');
-  if (imageTracker =="s") {
-    image.src = "img/r4.jpg";
-    imageTracker = "r";
-  } else {
-    image.src = "img/star1.jpg";
-    imageTracker = "s";
-  }
-}
-var imageTracker = "s";
-function switchImg5() {
-  var image = document.getElementById('box5');
-  if (imageTracker =="s") {
-    image.src = "img/r5.jpg";
-    imageTracker = "r";
-  } else {
-    image.src = "img/star1.jpg";
-    imageTracker = "s";
-  }
-}
-var imageTracker = "s";
-function switchImg6() {
-  var image = document.getElementById('box6');
-  if (imageTracker =="s") {
-    image.src = "img/r6.jpg";
-    imageTracker = "r";
-  } else {
-    image.src = "img/star1.jpg";
-    imageTracker = "s";
-  }
-}
-var imageTracker = "s";
-function switchImg7() {
-  var image = document.getElementById('box7');
-  if (imageTracker =="s") {
-    image.src = "img/r7.jpg";
-    imageTracker = "r";
-  } else {
-    image.src = "img/star1.jpg";
-    imageTracker = "s";
-  }
-}
-var imageTracker = "s";
-function switchImg8() {
-  var image = document.getElementById('box8');
-  if (imageTracker =="s") {
-    image.src = "img/r8.jpg";
-    imageTracker = "r";
-  } else {
-    image.src = "img/star1.jpg";
-    imageTracker = "s";
-  }
-}
-var imageTracker = "s";
-function switchImg9() {
-  var image = document.getElementById('box9');
-  if (imageTracker =="s") {
-    image.src = "img/r9.jpg";
-    imageTracker = "r";
-  } else {
-    image.src = "img/star1.jpg";
-    imageTracker = "s";
-  }
-}
-var imageTracker = "s";
-function switchImg10() {
-  var image = document.getElementById('box10');
-  if (imageTracker =="s") {
-    image.src = "img/r10.jpg";
-    imageTracker = "r";
-  } else {
-    image.src = "img/star1.jpg";
-    imageTracker = "s";
-  }
-}
-var imageTracker = "s";
-function switchImg11() {
-  var image = document.getElementById('box11');
-  if (imageTracker =="s") {
-    image.src = "img/r11.jpg";
-    imageTracker = "r";
-  } else {
-    image.src = "img/star1.jpg";
-    imageTracker = "s";
-  }
-}
-var imageTracker = "s";
-function switchImg12() {
-  var image = document.getElementById('box12');
-  if (imageTracker =="s") {
-    image.src = "img/r12.jpg";
-    imageTracker = "r";
-  } else {
-    image.src = "img/star1.jpg";
-    imageTracker = "s";
-  }
-}
+// Box 2
+  var imgS = "img/star1.jpg";
+  $('#box2').mousedown(function () {
+    $(this).attr("src","img/r2.jpg");
+  });
+  $('#box2').mouseup(function () {
+    $(this).attr("src",imgS);
+  });
+
+// Box 3
+  var imgS = "img/star1.jpg";
+  $('#box3').mousedown(function () {
+    $(this).attr("src","img/r3.jpg");
+  });
+  $('#box3').mouseup(function () {
+    $(this).attr("src",imgS);
+  });
+
+// Box 4
+  var imgS = "img/star1.jpg";
+  $('#box4').mousedown(function () {
+    $(this).attr("src","img/r4.jpg");
+  });
+  $('#box4').mouseup(function () {
+    $(this).attr("src",imgS);
+  });
+
+// Box 5
+  var imgS = "img/star1.jpg";
+  $('#box5').mousedown(function () {
+    $(this).attr("src","img/r5.jpg");
+  });
+  $('#box5').mouseup(function () {
+    $(this).attr("src",imgS);
+  });
+
+// Box 6
+  var imgS = "img/star1.jpg";
+  $('#box6').mousedown(function () {
+    $(this).attr("src","img/r6.jpg");
+  });
+  $('#box6').mouseup(function () {
+    $(this).attr("src",imgS);
+  });
+
+// Box 7
+  var imgS = "img/star1.jpg";
+  $('#box7').mousedown(function () {
+    $(this).attr("src","img/r7.jpg");
+  });
+  $('#box7').mouseup(function () {
+    $(this).attr("src",imgS);
+  });
+
+// Box 8
+  var imgS = "img/star1.jpg";
+  $('#box8').mousedown(function () {
+    $(this).attr("src","img/r8.jpg");
+  });
+  $('#box8').mouseup(function () {
+    $(this).attr("src",imgS);
+  });
+
+// Box 9
+  var imgS = "img/star1.jpg";
+  $('#box9').mousedown(function () {
+    $(this).attr("src","img/r9.jpg");
+  });
+  $('#box9').mouseup(function () {
+    $(this).attr("src",imgS);
+  });
+
+// Box 10
+  var imgS = "img/star1.jpg";
+  $('#box10').mousedown(function () {
+    $(this).attr("src","img/r10.jpg");
+  });
+  $('#box10').mouseup(function () {
+    $(this).attr("src",imgS);
+  });
+
+// Box 11
+  var imgS = "img/star1.jpg";
+  $('#box11').mousedown(function () {
+    $(this).attr("src","img/r11.jpg");
+  });
+  $('#box11').mouseup(function () {
+    $(this).attr("src",imgS);
+  });
+
+// Box 12
+  var imgS = "img/star1.jpg";
+  $('#box12').mousedown(function () {
+    $(this).attr("src","img/r12.jpg");
+  });
+  $('#box12').mouseup(function () {
+    $(this).attr("src",imgS);
+  });
+
+  // $('#box1').mouseenter(function(){
+  //   $(this).attr("src","img/r1.jpg");
+  // });
+  // $('#box1').mouseleave(function(){
+  //   $(this).attr("src",imgS);
+  // });
+});
+
+// var imageTracker = "s";
+// function switchImg() {
+//   var image = document.getElementById('box1');
+//   if (imageTracker =="s") {
+//     image.src = "img/r1.jpg";
+//     imageTracker = "r";
+//   } else {
+//     image.src = "img/star1.jpg";
+//     imageTracker = "s";
+//   }
+// }
+// var imageTracker = "s";
+// function switchImg2() {
+//   var image = document.getElementById('box2');
+//   if (imageTracker =="s") {
+//     image.src = "img/r2.jpg";
+//     imageTracker = "r";
+//   } else {
+//     image.src = "img/star1.jpg";
+//     imageTracker = "s";
+//   }
+// }
+// var imageTracker = "s";
+// function switchImg3() {
+//   var image = document.getElementById('box3');
+//   if (imageTracker =="s") {
+//     image.src = "img/r3.jpg";
+//     imageTracker = "r";
+//   } else {
+//     image.src = "img/star1.jpg";
+//     imageTracker = "s";
+//   }
+// }
+// var imageTracker = "s";
+// function switchImg4() {
+//   var image = document.getElementById('box4');
+//   if (imageTracker =="s") {
+//     image.src = "img/r4.jpg";
+//     imageTracker = "r";
+//   } else {
+//     image.src = "img/star1.jpg";
+//     imageTracker = "s";
+//   }
+// }
+// var imageTracker = "s";
+// function switchImg5() {
+//   var image = document.getElementById('box5');
+//   if (imageTracker =="s") {
+//     image.src = "img/r5.jpg";
+//     imageTracker = "r";
+//   } else {
+//     image.src = "img/star1.jpg";
+//     imageTracker = "s";
+//   }
+// }
+// var imageTracker = "s";
+// function switchImg6() {
+//   var image = document.getElementById('box6');
+//   if (imageTracker =="s") {
+//     image.src = "img/r6.jpg";
+//     imageTracker = "r";
+//   } else {
+//     image.src = "img/star1.jpg";
+//     imageTracker = "s";
+//   }
+// }
+// var imageTracker = "s";
+// function switchImg7() {
+//   var image = document.getElementById('box7');
+//   if (imageTracker =="s") {
+//     image.src = "img/r7.jpg";
+//     imageTracker = "r";
+//   } else {
+//     image.src = "img/star1.jpg";
+//     imageTracker = "s";
+//   }
+// }
+// var imageTracker = "s";
+// function switchImg8() {
+//   var image = document.getElementById('box8');
+//   if (imageTracker =="s") {
+//     image.src = "img/r8.jpg";
+//     imageTracker = "r";
+//   } else {
+//     image.src = "img/star1.jpg";
+//     imageTracker = "s";
+//   }
+// }
+// var imageTracker = "s";
+// function switchImg9() {
+//   var image = document.getElementById('box9');
+//   if (imageTracker =="s") {
+//     image.src = "img/r9.jpg";
+//     imageTracker = "r";
+//   } else {
+//     image.src = "img/star1.jpg";
+//     imageTracker = "s";
+//   }
+// }
+// var imageTracker = "s";
+// function switchImg10() {
+//   var image = document.getElementById('box10');
+//   if (imageTracker =="s") {
+//     image.src = "img/r10.jpg";
+//     imageTracker = "r";
+//   } else {
+//     image.src = "img/star1.jpg";
+//     imageTracker = "s";
+//   }
+// }
+// var imageTracker = "s";
+// function switchImg11() {
+//   var image = document.getElementById('box11');
+//   if (imageTracker =="s") {
+//     image.src = "img/r11.jpg";
+//     imageTracker = "r";
+//   } else {
+//     image.src = "img/star1.jpg";
+//     imageTracker = "s";
+//   }
+// }
+// var imageTracker = "s";
+// function switchImg12() {
+//   var image = document.getElementById('box12');
+//   if (imageTracker =="s") {
+//     image.src = "img/r12.jpg";
+//     imageTracker = "r";
+//   } else {
+//     image.src = "img/star1.jpg";
+//     imageTracker = "s";
+//   }
+// }
 
 //Attempt at assigning images to an array
 //then randomize the order they appear at page reload.
